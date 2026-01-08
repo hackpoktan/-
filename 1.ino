7ad0fe606b55e4dabbb4f725b9c6f3577e397ae7
hackpoktan <boss20088002@gmail.com>
2026-01-08 12:21:52 +0900

    Update 1.ino

--- 1.ino
+++ 1.ino
@@ -1,43 +1,48 @@
-#define UART_TX 8
-#define UART_BAUD 9600
-#define BIT_US (1000000UL / UART_BAUD)
+/*
+ * Bit-Bang UART TX on D8 (PORTB0)
+ * Send character 'a' repeatedly at 9600 bps
+ * Observe waveform on Oscilloscope CH1 -> D8
+ */
 
-void uartSendChar(char data) {
-  // Start bit
-  digitalWrite(UART_TX, LOW);
-  delayMicroseconds(BIT_US);
+#define BAUD_RATE 9600
+#define BIT_DELAY_US (1000000UL / BAUD_RATE) // ~104 us
+
+// D8 = PORTB bit 0
+#define TX_BIT 0
+
+#define TX_HIGH() (PORTB |= (1 << TX_BIT))
+#define TX_LOW()  (PORTB &= ~(1 << TX_BIT))
+
+void bitDelay() {
+  delayMicroseconds(BIT_DELAY_US);
+}
+
+void sendByte(uint8_t data) {
+  // Start bit (LOW)
+  TX_LOW();
+  bitDelay();
 
   // Data bits (LSB first)
-  for (uint8_t bit = 0; bit < 8; bit++) {
-    if (data & (1 << bit))
-      digitalWrite(UART_TX, HIGH);
-    else
-      digitalWrite(UART_TX, LOW);
-
-    delayMicroseconds(BIT_US);
+  for (int i = 0; i < 8; i++) {
+    if (data & (1 << i)) TX_HIGH();
+    else TX_LOW();
+    bitDelay();
   }
 
-  // Stop bit
-  digitalWrite(UART_TX, HIGH);
-  delayMicroseconds(BIT_US);
+  // Stop bit (HIGH)
+  TX_HIGH();
+  bitDelay();
 }
 
 void setup() {
-  pinMode(UART_TX, OUTPUT);
-  digitalWrite(UART_TX, HIGH);   // Idle state
+  // Set D8 (PORTB0) as output
+  DDRB |= (1 << TX_BIT);
 
-  Serial.begin(9600);
-  Serial.println("[SP01] UART waveform test started");
-  Serial.print("Baud rate: ");
-  Serial.println(UART_BAUD);
-  Serial.print("Bit time (us): ");
-  Serial.println(BIT_US);
-
-  delay(1500); // scope 준비 시간
+  // Idle state = HIGH
+  TX_HIGH();
 }
 
 void loop() {
-  uartSendChar('a');
-  delay(400);  // 반복 간격
+  sendByte('a');      // 0x61
+  delay(10);          // 간격 주기 (파형 구분용)
 }
-
